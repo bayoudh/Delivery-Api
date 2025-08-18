7d24238209e573229ec129f89aa716a0476a10c5
Mohamed Salah Bayoudh <mohamed.salah.bayoudh@gmail.com>
2025-08-18 18:42:20 +0100

    update

--- dist/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const connectDB = async () => {
-    if (!process.env.MONGO_URI)
-        throw new Error("MONGO_URI is not set");
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected");
-};
-export default connectDB;
